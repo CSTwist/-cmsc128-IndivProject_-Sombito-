e30757ffed253b5f68120a5ab4afda62258877b1
Chakinzo Sombito <schakinzo@gmail.com>
2025-09-26 17:28:49 +0800

    Added additional functionalities

--- backend.py
+++ backend.py
@@ -1,14 +1,43 @@
-from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask import Flask, jsonify, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.permanent_session_lifetime = timedelta(days=5)
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=True)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    if "user" in session:     
+        return render_template("index.html")
+    else:
+        flash("You are not logged in!", "danger")
+        return redirect(url_for("login"))
 
+@app.route("/view")
+def view():
+    return render_template("view.html", values=users.query.all())
+
+@app.route("/profile")
+def profile():
+    if "user" in session:     
+        return render_template("profile.html", user=session["user"])
+    else:
+        flash("You are not logged in!", "danger")
+        return redirect(url_for("login"))
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -16,28 +45,62 @@ def login():
         session.permanent = True
         user = request.form["nm"]
         session["user"] = user
+        
+        found_user = users.query.filter_by(name=user).first()
+        if found_user:
+            session["email"] = found_user.email
+            
+        else:
+            usr = users(user, "")
+            db.session.add(usr)
+            db.session.commit()
+        
         flash("Login Successful!", "info")
-        return redirect(url_for("user"))
+        return redirect(url_for("home"))
     else:
         if "user" in session:
             flash("Already Logged In!", "info")
             return redirect(url_for("user"))
         return render_template("login.html")
  
-@app.route("/user")
+@app.route("/user", methods=["POST","GET"])
 def user():
+    email = None
     if "user" in session:
         user = session["user"]
-        return render_template("user.html", user=user)
+        
+        if request.method == "POST":
+                email = request.form["email"]
+                session["email"] = email
+                found_user = users.query.filter_by(name=user).first()
+                found_user.email = email
+                db.session.commit()
+                flash("Email was saved!", "info")
+        else:
+            if "email" in session:
+                email = session["email"]        
+        
+        return render_template("user.html", email=email)
     else:
         flash("You are not logged in!", "danger")
         return redirect(url_for("login"))
+    
+@app.route("/add_task", methods=["POST"])
+def add_task():
+    taskName = request.form['taskName']
+    deadline = request.form['deadline']
+    # code for the database 
+    return jsonify({'taskName' : taskName, 'deadline' : deadline})
 
 @app.route("/logout")
 def logout():
+    
     flash(f"You have been logged out!", "info")
     session.pop("user", None)
+    session.pop("email", None)
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
